86e93e1fca575e019dca03c81d1bcea8b2043a1c
MasterMindAlex01 <30800345+MasterMindAlex01@users.noreply.github.com>
2024-09-18 10:42:14 -0500

    Integrations tests

---
+++ tests/SuppliersManager.IntegrationTesting/CustomWebApplicationFactory.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Mongo2Go;
+using MongoDB.Driver;
+using SuppliersManager.Application.Models.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuppliersManager.IntegrationTesting
+{
+    public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        private readonly MongoDbRunner _mongoRunner;
+
+        public CustomWebApplicationFactory()
+        {
+            _mongoRunner = MongoDbRunner.Start(); // Iniciar MongoDB en memoria
+            _mongoRunner.Import("suppliersdb", "users", "suppliersdb.users.json", false);
+            _mongoRunner.Import("suppliersdb", "suppliers", "suppliersdb.suppliers.json", false);
+        }
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(async services =>
+            {
+                // Reemplazar la configuración de MongoDB para pruebas
+                services.Configure<MongoDBSettings>(options =>
+                {
+                    options.ConnectionURI = _mongoRunner.ConnectionString;
+                    options.DatabaseName = "TestDatabase";
+                });
+
+                // Crear el cliente y el contexto de MongoDB en memoria
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+                var database = scope.ServiceProvider.GetRequiredService<IMongoDatabase>();
+                List<string> names = new List<string>();
+                var cursor = await database.ListCollectionNamesAsync();  // Asegurar que la base de datos está vacía
+                await cursor.ForEachAsync(x => names.Add(x));
+                var taskDropCollection = new List<Task>();
+                foreach (var name in names)
+                {
+                    taskDropCollection.Add(database.DropCollectionAsync(name));
+                }
+                await Task.WhenAll(taskDropCollection);
+            });
+        }
+
+        //public void Dispose()
+        //{
+        //    _mongoRunner.Dispose(); // Detener MongoDB en memoria al finalizar las pruebas
+        //    base.Dispose();
+        //}
+    }
+}
---
+++ tests/SuppliersManager.IntegrationTesting/Suppliers/SuppliersEndpointTests.cs
@@ -0,0 +1,67 @@
+﻿
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using SuppliersManager.Api;
+using SuppliersManager.Application.Features.Suppliers.Commands;
+using SuppliersManager.Shared.Wrapper;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace SuppliersManager.IntegrationTesting.Suppliers
+{
+    public class SuppliersEndpointTests : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private const string urlController = "/v1/Suppliers";
+        private readonly HttpClient _client;
+
+        public SuppliersEndpointTests(CustomWebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetAllSuppliers_ReturnsOkResponse()
+        {
+            // Arrange
+            var response = await _client.GetAsync($"{urlController}/GetAll?pageNumber=1&pagesize=10");
+
+            // Act
+            response.EnsureSuccessStatusCode();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(content);
+        }
+
+        [Fact]
+        public async Task CreateSupplier_ReturnsCreatedResponse()
+        {
+            // Arrange
+            var newSupplier = new CreateSupplierCommand()
+            {
+                Address = "cra 50 # 45- 50",
+                City = "Medellin",
+                Email = "alex@example.com",
+                RegisteredName = "EmpresaTest",
+                State = "Antioquia",
+                TIN = "9825255245-45"
+            };
+            var content = new StringContent(JsonSerializer.Serialize(newSupplier), Encoding.UTF8, "application/json");
+
+            // Act
+            var response = await _client.PostAsync($"{urlController}/create", content);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var createdId = JsonSerializer.Deserialize<Result<string>>(responseContent);
+            var result = await Result<string>.SuccessAsync();
+
+            Assert.NotEqual(result, createdId);
+        }
+    }
+}
